420523b20d24fbd471de3d4de01cd4567030970c
Jonathan Choi <jisung594@gmail.com>
2019-07-16 02:12:24 -0400

    need to clean up

--- index.js
+++ index.js
@@ -23,8 +23,12 @@ const projects = [
   }
 ]
 
-app.get('/', (req,res) => {
-  res.send('Hello World')
+// app.get('/', (req,res) => {
+//   res.send('Hello World')
+// })
+
+app.get('/express-backend', (req,res) => {
+  res.send({express: 'Express backend is now connected to React frontend!'})
 })
 
 app.get('/api/projects', (req,res) => {
@@ -82,5 +86,5 @@ app.post('/api/projects', (req,res) => {
 
 
 // PORT
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Listening on port ${port}`))
