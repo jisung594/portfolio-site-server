5c01a52943c755830f3871568ea1fa0afc619a59
Jonathan Choi <jisung594@gmail.com>
2019-07-27 12:16:15 -0400

    working?

--- index.js
+++ index.js
@@ -189,8 +189,8 @@ app.post('/api/projects', (req,res) => {
 
 
 // PORT
-// const port = process.env.PORT || "5000"
-// app.listen(port, () => console.log(`Listening on port ${port}`))
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Listening on port ${port}`))
 
 
 
