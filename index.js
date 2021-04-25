005c075886b6a4e88b870bcd753b5e41d0113a42
Jonathan Choi <jisung594@gmail.com>
2021-04-24 20:05:47 -0500

    minor

--- index.js
+++ index.js
@@ -2,43 +2,7 @@ const express = require('express');
 const Joi = require('joi');
 const app = express();
 
-
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// var app = express();
-//
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hjs');
-//
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-
-// ------------------------------------
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors());
 
 app.use(function(req, res, next) {
@@ -61,7 +25,7 @@ const projects = [
     },
     {
       id: 2,
-      name: "Interactive Periodic Table (WIP)",
+      name: "Interactive Periodic Table",
       url: "http://iptable.herokuapp.com/",
       img: "https://i.imgur.com/V2wO2DM.png",
       type: "dev",
